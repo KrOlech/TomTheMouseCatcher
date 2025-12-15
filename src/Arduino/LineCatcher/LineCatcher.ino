aa961d7a8fbdd79a7cf3a231db65b4598f8d1497
Zenbook <Krzysztof_olech@interia.pl>
2025-12-15 13:59:58 +0100

    Simple arduino code to order line engie to follow the mice

---
+++ src/Arduino/LineCatcher/LineCatcher.ino
@@ -0,0 +1,69 @@
+#define X_EN_PIN 29//3
+#define X_STEP_PIN 6//4  //B6 or A1
+#define X_DIR_PIN 7//5   //B5 or A2
+#define X_END_PIN 0//6   //B4 or A3
+
+
+/*
+Zones:
+
+H1 G1 F1 S1
+         E1
+A B C D
+         E2
+H2 G2 F2 S2
+
+0 1 2 3
+      3
+0 1 2 3
+      3
+0 1 2 3
+
+*/
+
+int8_t current_zone = 0;
+
+void setup() {
+Serial.begin(9600);
+pinMode(X_DIR_PIN, OUTPUT);
+pinMode(X_STEP_PIN, OUTPUT);
+pinMode(X_EN_PIN, OUTPUT);
+
+
+}
+
+
+
+void loop() {
+  String readString = "";
+
+  while (Serial.available()) {
+    delay(3);  //delay to allow buffer to fill
+    if (Serial.available() >0) {
+      char c = Serial.read();  //gets one byte from serial buffer
+      readString += c; //makes the string readString
+    }
+  }
+
+  int newZone = readString.toInt();
+
+  int zoneDelta = current_zone - newZone;
+
+  if (zoneDelta > 0){
+        digitalWrite(X_DIR_PIN, LOW);
+        digitalWrite(X_EN_PIN, LOW);
+  }
+  else if(zoneDelta < 0){
+          digitalWrite(X_DIR_PIN, HIGH);
+          digitalWrite(X_EN_PIN, LOW);
+
+  } else {
+        digitalWrite(X_EN_PIN, HIGH);
+  }
+
+  digitalWrite(X_STEP_PIN, HIGH);
+  delay(10);
+  digitalWrite(X_STEP_PIN, LOW);
+  delay(10);
+
+}
