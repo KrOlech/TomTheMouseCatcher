4b8967e4c3bb41ea243d55a9847948b2a25c0cf8
Zenbook <Krzysztof_olech@interia.pl>
2026-01-22 12:16:06 +0100

    Saving last status that was send and write to wirtual carrage to clinup the log and do not overload the com port

--- src/Python/VirtualCarrage/VirtualCarrage.py
+++ src/Python/VirtualCarrage/VirtualCarrage.py
@@ -19,6 +19,8 @@ class VirtualCarrage(Loger):
         if Settings.arduinoLineCome:
             self.arduino = serial.Serial(port=Settings.arduinoLineCome, baudrate=Settings.baudrate, timeout=.1)
 
+    last_status = None
+
     def advance(self, zoneCords):
 
         x0, y0, w, h = zoneCords
@@ -26,23 +28,31 @@ class VirtualCarrage(Loger):
         if self.position < x0 + w / 2 - self.SPEED // 2:
             # right
             self.position += self.SPEED
-            self.loger("moving virtual carriage to the right")
-            if Settings.arduinoLineCome:
-                self.loger("saving commend to Arduino")
-                self.arduino.write(bytes("1", 'utf-8'))
+            if self.last_status != "right":
+                self.last_status = "right"
+                self.loger("moving virtual carriage to the right")
+                if Settings.arduinoLineCome:
+                    self.loger("saving commend to Arduino")
+                    self.arduino.write(bytes("1", 'utf-8'))
+
         elif self.position > x0 + w / 2 + self.SPEED // 2:
             # left
             self.position -= self.SPEED
-            self.loger("moving virtual carriage to the Left")
-            if Settings.arduinoLineCome:
-                self.loger("saving commend to Arduino")
-                self.arduino.write(bytes("-1", 'utf-8'))
+            if self.last_status != "Left":
+                self.last_status = "Left"
+                self.loger("moving virtual carriage to the Left")
+                if Settings.arduinoLineCome:
+                    self.loger("saving commend to Arduino")
+                    self.arduino.write(bytes("-1", 'utf-8'))
+
         else:
             # stop
-            self.loger("Stoping virtual carriage")
-            if Settings.arduinoLineCome:
-                self.loger("saving commend to Arduino")
-                self.arduino.write(bytes("100", 'utf-8'))
+            if self.last_status != "stop":
+                self.last_status = "stop"
+                self.loger("Stoping virtual carriage")
+                if Settings.arduinoLineCome:
+                    self.loger("saving commend to Arduino")
+                    self.arduino.write(bytes("100", 'utf-8'))
 
         if self.position > self.MAZE_LENGTH_PIZELS:
             self.position = self.MAZE_LENGTH_PIZELS
